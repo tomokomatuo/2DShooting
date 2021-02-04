9d760de3d80394e8849aeaddb163a0e4cbab319a
tomokomatuo <matsuo09212001@yahoo.co.jp>
2021-02-04 16:41:10 +0900

    phase実装

---
+++ Assets/Scripts/EnemyScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyScript : MonoBehaviour
+{
+    private float phase;
+    // Start is called before the first frame update
+    void Start()
+    {
+      phase = Random.Range(0f, Mathf.PI *2);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += new Vector3(
+            Mathf.Cos(Time.frameCount * 0.05f + phase) * 0.05f,
+        　　 -2f * Time.deltaTime,
+        　　 0f);
+    }
+}
---
+++ Assets/Scripts/GameControllerScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameControllerScript : MonoBehaviour
+{
+    public GameObject enemy;
+    // Start is called before the first frame update
+    IEnumerator SpawnEnemy()
+    {
+　　　　　while(true)
+
+
+    {
+      Instantiate(
+          enemy,
+          new Vector3(Random.Range(-12.8f, 12.8f), transform.position.y, 0f),
+          transform.rotation
+          );
+          yield return new WaitForSeconds(0.5f);
+    }
+    }
+    void Start()
+    {
+        StartCoroutine("SpawnEnemy");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
